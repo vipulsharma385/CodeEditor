faf51b3aa160a36e369b34f528a94d6dbb0246b6
vipulsharma385 <vs0056095@gmail.com>
2022-05-26 15:49:15 +0530

    CHECK

--- codeeditor/src/App.js
+++ codeeditor/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ADITYA Edit <code>src/App.js</code> and save to reload.
+          ADITYA VIPUL Edit <code>src/App.js</code> and save to reload.
         </p>
        
       </header>
